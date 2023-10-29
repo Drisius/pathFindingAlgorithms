fe2eaf424960926f8ed31e012df5b8a3d2a836eb
Drisius <33498094+Drisius@users.noreply.github.com>
2023-10-29 22:32:12 +0100

    Borrowed rendering code from the wfcGen project to get a minimal working setup.

--- pathFindingAlgorithms/main.cpp
+++ pathFindingAlgorithms/main.cpp
@@ -1,6 +1,41 @@
+#define SDL_MAIN_HANDLED
+
+#include <SDL2/SDL.h>
+
 #include <iostream>
+#include <time.h> 
+#include <vector>
+
+#include "main.h"
+
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")		
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	srand((unsigned int)time(NULL));
+
+	SDL_Event event;
+	SDL_Renderer* renderer;
+	SDL_Window* window;
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_CreateWindowAndRenderer(SPRITE_SIZE * X_DIMENSION, SPRITE_SIZE * Y_DIMENSION, 0, &window, &renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderClear(renderer);
+
+
+
+
+
+	SDL_RenderPresent(renderer);
+
+	while (1) {
+		if (SDL_PollEvent(&event) && event.type == SDL_QUIT)
+			break;
+	}
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
 }
